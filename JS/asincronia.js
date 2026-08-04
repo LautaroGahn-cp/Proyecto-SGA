d87c7f9c6227600e6da0ef20ed9970c443904f7e
LautaroGahn-cp <lautaro.gahn@gmail.com>
2026-08-04 15:00:10 -0300

    Clase 02- agregar estructura JS. Agregando sistema de asincronia.

---
+++ JS/asincronia.js
@@ -0,0 +1,41 @@
+/*console.log("Inicio");
+setTimeout(() => {
+    console.log("Buscar alumnos");
+}, 3000);
+console.log("Fin");
+
+function saludar(){
+    console.log ("hola")
+}
+
+function ejecutar (funcion){
+    funcion();
+}
+
+ejecutar (saludar)
+
+function despedirse () {
+    console.log("hasta luego")
+}
+
+setTimeout(despedirse, 3000);
+
+setTimeout(() => {
+   console.log("Buscando docentes...") 
+}, 2000);
+
+setTimeout(() => {
+   console.log("buscando materias") 
+}, 4000);
+
+setTimeout(() => {
+   console.log("Buscar el curso...") 
+}, 1000);*/
+
+console.log("abrimos SGA");
+
+setTimeout(() => {
+   console.log("Alumnos cargados...") 
+}, 2000);
+
+console.log("El usuario puede seguir navegando");
